635063fc1af8c15f0e5052dbace73fc547c3a1a4
boris1592 <buzin.boria@gmail.com>
2024-04-01 00:16:23 +0300

    dry

--- src/util.js
+++ src/util.js
@@ -2,19 +2,25 @@ const { EmbedBuilder } = require("discord.js");
 
 /**
  * @param {import('discord.js').Interaction} interaction
+ * @param {string | import('discord.js').MessagePayload | import('discord.js').InteractionReplyOptions} options
+ */
+async function replyOrFollowup(interaction, options) {
+  if (interaction.replied || interaction.deferred) {
+    await interaction.followUp(options);
+  } else {
+    await interaction.reply(options);
+  }
+}
+
+/**
+ * @param {import('discord.js').Interaction} interaction
  * @returns {(message: string) => Promise<void>}
  */
 function fancyReply(interaction) {
   return async (message) => {
-    const options = {
+    await replyOrFollowup(interaction, {
       embeds: [new EmbedBuilder().setColor("#2ecc71").setDescription(message)],
-    };
-
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp(options);
-    } else {
-      await interaction.reply(options);
-    }
+    });
   };
 }
 
@@ -24,16 +30,10 @@ function fancyReply(interaction) {
  */
 function fancyError(interaction) {
   return async (message) => {
-    const options = {
+    await replyOrFollowup(interaction, {
       embeds: [new EmbedBuilder().setColor("#e74c3c").setDescription(message)],
       ephemeral: true,
-    };
-
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp(options);
-    } else {
-      await interaction.reply(options);
-    }
+    });
   };
 }
 
