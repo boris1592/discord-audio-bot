146af4924074aefb3fac222e2ead43741480cab2
boris1592 <buzin.boria@gmail.com>
2024-04-05 18:56:09 +0300

    Classes are overrated

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,16 +1,12 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export class PingCommand {
-  constructor() {
-    this.info = new SlashCommandBuilder()
-      .setName("ping")
-      .setDescription("Ping!");
-  }
+export const info = new SlashCommandBuilder()
+  .setName("ping")
+  .setDescription("Ping!");
 
-  /**
-   * @param {{reply: (message: string) => Promise<void>}}
-   */
-  async execute({ reply }) {
-    await reply("Pong!");
-  }
+/**
+ * @param {{reply: (message: string) => Promise<void>}}
+ */
+export async function execute({ reply }) {
+  await reply("Pong!");
 }
